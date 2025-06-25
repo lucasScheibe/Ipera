695f1b1d3f4fdfc54162493c4fb710d318b1e785
Lucas Gabriel Scheibe <lucas.scheibe@catolicasc.edu.br>
2025-06-25 17:02:10 -0300

    Delete Ipera-main directory

--- Ipera-main/Ipera-main/js/pagamentos/confirmação pagamento.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleBtn = document.getElementById("darkModeToggle");
-
-  toggleBtn.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-  });
-});
