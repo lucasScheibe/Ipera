35c1cf2f06d0b379e28842cbdc74ef73cb256cbf
Lucas <lucas.scheibe@catolicasc.edu.br>
2025-06-24 23:40:04 -0300

    ajustes dark mode

--- js/login.js
+++ js/login.js
@@ -1,41 +1,30 @@
-<<<<<<< HEAD
-const toggle = document.getElementById('darkModeToggle');
-const body = document.body;
+document.getElementById("btnLogin").addEventListener("click", function () {
+    const nome = document.getElementById("nomeUsuario").value.trim();
+    const email = document.getElementById("emailUsuario").value.trim();
 
-// Verifica se o modo escuro já estava ativado no localStorage
-if (localStorage.getItem('modoEscuro') === 'true') {
-  body.classList.add('dark-mode');
-  toggle.checked = true;
-}
+    if (nome === "" || email === "") {
+      alert("Por favor, preencha nome e e-mail.");
+      return;
+    }
 
-// Escuta o clique no botão
-toggle.addEventListener('change', function () {
-  if (this.checked) {
+    localStorage.setItem("usuarioLogado", nome);
+    window.location.href = "main.html";
+  });
+
+  const toggle = document.getElementById('darkModeToggle');
+  const body = document.body;
+
+  if (localStorage.getItem('modoEscuro') === 'true') {
     body.classList.add('dark-mode');
-    localStorage.setItem('modoEscuro', 'true');
-  } else {
-    body.classList.remove('dark-mode');
-    localStorage.setItem('modoEscuro', 'false');
+    toggle.checked = true;
   }
-});
-=======
-const toggle = document.getElementById('darkModeToggle');
-const body = document.body;
 
-// Verifica se o modo escuro já estava ativado no localStorage
-if (localStorage.getItem('modoEscuro') === 'true') {
-  body.classList.add('dark-mode');
-  toggle.checked = true;
-}
-
-// Escuta o clique no botão
-toggle.addEventListener('change', function () {
-  if (this.checked) {
-    body.classList.add('dark-mode');
-    localStorage.setItem('modoEscuro', 'true');
-  } else {
-    body.classList.remove('dark-mode');
-    localStorage.setItem('modoEscuro', 'false');
-  }
-});
->>>>>>> 604cd9b9af1b5ea3cd56741b81189516ce9cacfd
+  toggle.addEventListener('change', function () {
+    if (this.checked) {
+      body.classList.add('dark-mode');
+      localStorage.setItem('modoEscuro', 'true');
+    } else {
+      body.classList.remove('dark-mode');
+      localStorage.setItem('modoEscuro', 'false');
+    }
+  });
